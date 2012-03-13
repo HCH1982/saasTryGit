dec54e5c324107652b33503b697834613d517e35
Hank Huang <Hanchien.h@gmail.com>
2012-03-14 01:01:36 +0800

    HOME

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
   def index
     if params[:sort] == "title"
        @movies = Movie.order(:title).all
-       flash[:sortCol] =  "hilite" 
+       render :sorCol =>  "hilite" 
     elsif params[:sort] == "date"
        @movies = Movie.order(:release_date).all
     else 
