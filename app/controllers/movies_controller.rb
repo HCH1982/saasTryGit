4575a6fd6bf70b2870a0c610e19e0c545a8ecd4b
Hank <hanchien.h@gmail.com>
2012-03-14 14:54:00 +0800

    ci

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-       @all_ratings = Movie.find(:all, :select => 'rating').map(&:rating).uniq
+       @all_ratings = Movie.ratings
     if params[:sort] == "title"
        @movies = Movie.order(:title).all
        @sorColTitle = "hilite" 
